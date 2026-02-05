31a9ef2c97176a91d214f79c07a3be1558166f5c
José Pazmiño <josepf.dev@gmail.com>
2026-02-05 00:51:10 -0500

    feat: refactor DropZone component to use Tabler icons and improve file handling

--- src/components/DropZone.tsx
+++ src/components/DropZone.tsx
@@ -1,33 +1,114 @@
-import { Group, Text } from '@mantine/core';
-import { type DropzoneProps, Dropzone as MantineDropZone } from '@mantine/dropzone';
-import { Image, Upload, X } from "lucide-react";
+import { ActionIcon, Group, Text } from "@mantine/core";
+import {
+  Dropzone as MantineDropZone,
+  type DropzoneProps as MantineDropzoneProps,
+} from "@mantine/dropzone";
+import {
+  IconCheckbox,
+  IconPhoto,
+  IconUpload,
+  IconX,
+} from "@tabler/icons-react";
+import { MouseEvent, useRef, useState } from "react";
 
-export function DropZone(props: Partial<DropzoneProps>) {
+interface DropZoneProps extends Partial<MantineDropzoneProps> {
+  label: string;
+  description?: string;
+  onFileSelect?: (file: File | null) => void;
+}
+
+const MIN_HEIGHT = 220;
+const MAX_FILES = 1;
+const ICON_SIZE = 52;
+const STROKE = 1.5;
+
+export function DropZone({
+  label,
+  description,
+  onFileSelect,
+  ...props
+}: DropZoneProps) {
+  const [file, setFile] = useState<File | null>(null);
+  const openRef = useRef<() => void>(null);
+
+  const handleDrop = (files: File[]) => {
+    setFile(files[0]);
+    onFileSelect?.(files[0]);
+  };
+
+  const handleRemoveFile = (e: MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    setFile(null);
+    onFileSelect?.(null);
+  };
+
   return (
     <MantineDropZone
-      onDrop={(files) => console.log('accepted files', files)}
-      onReject={(files) => console.log('rejected files', files)}
+      openRef={openRef}
+      maxFiles={MAX_FILES}
+      onDrop={handleDrop}
       {...props}
     >
-      <Group justify="center" gap="xl" mih={220} style={{ pointerEvents: 'none' }}>
+      <Group
+        justify="center"
+        gap="xl"
+        mih={MIN_HEIGHT}
+        style={{ pointerEvents: "none" }}
+      >
         <MantineDropZone.Accept>
-          <Upload size={52} color="var(--mantine-color-blue-6)" stroke="1.5" />
+          <IconUpload
+            size={ICON_SIZE}
+            color="var(--mantine-color-blue-6)"
+            stroke={STROKE}
+          />
         </MantineDropZone.Accept>
         <MantineDropZone.Reject>
-          <X size={52} color="var(--mantine-color-red-6)" stroke="1.5" />
+          <IconX
+            size={ICON_SIZE}
+            color="var(--mantine-color-red-6)"
+            stroke={STROKE}
+          />
         </MantineDropZone.Reject>
         <MantineDropZone.Idle>
-          <Image size={52} color="var(--mantine-color-dimmed)" stroke="1.5" />
+          {file ? (
+            <IconCheckbox
+              size={ICON_SIZE}
+              color="var(--mantine-color-green-6)"
+              stroke={STROKE}
+            />
+          ) : (
+            <IconPhoto
+              size={ICON_SIZE}
+              color="var(--mantine-color-dimmed)"
+              stroke={STROKE}
+            />
+          )}
         </MantineDropZone.Idle>
-
-        <div>
-          <Text size="xl" inline>
-            Drag images here or click to select files
-          </Text>
-          <Text size="sm" c="dimmed" inline mt={7}>
-            Attach as many files as you like, each file should not exceed 5mb
-          </Text>
-        </div>
+        {file ? (
+          <div className="flex items-start gap-4">
+            <Text size="sm" c="green" inline mt={7}>
+              {file.name}
+            </Text>
+            <ActionIcon
+              onClick={handleRemoveFile}
+              variant="filled"
+              color="red"
+              size="md"
+              style={{ pointerEvents: "all" }}
+            >
+              <IconX size={ICON_SIZE} stroke={STROKE} />
+            </ActionIcon>
+          </div>
+        ) : (
+          <div>
+            <Text size="xl" inline>
+              {label}
+            </Text>
+            <Text size="sm" c="dimmed" inline className="mt-2">
+              {description}
+            </Text>
+          </div>
+        )}
       </Group>
     </MantineDropZone>
   );
