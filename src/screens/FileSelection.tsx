72c27eddd7c972ea1e7932d67efabef825981c8d
José Pazmiño <josepf.dev@gmail.com>
2026-02-05 00:53:09 -0500

    feat: update FileSelection screen to enhance DropZone usage and improve file type handling

--- src/screens/FileSelection.tsx
+++ src/screens/FileSelection.tsx
@@ -1,7 +1,23 @@
+import {
+  MS_EXCEL_MIME_TYPE,
+  MS_WORD_MIME_TYPE,
+  PDF_MIME_TYPE,
+} from "@mantine/dropzone";
 import { DropZone } from "../components/DropZone";
 
 export default function FileSelection() {
-    return <div>
-        <DropZone />
+  return (
+    <div>
+      <DropZone
+        label="Seleccione o arrastre un archivo EXCEL aquí"
+        description="Solo se permite un archivo"
+        accept={MS_EXCEL_MIME_TYPE}
+      />
+      <DropZone
+        label="Seleccione o arrastre un archivo PDF o WORD aquí"
+        description="Solo se permite un archivo"
+        accept={[...MS_WORD_MIME_TYPE, ...PDF_MIME_TYPE]}
+      />
     </div>
+  );
 }
