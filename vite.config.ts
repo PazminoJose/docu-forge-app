1bbbb9f775b20794c7a10275d284076e47900eef
José Pazmiño <josepf.dev@gmail.com>
2026-02-09 17:45:34 -0500

    chore: add support for relative paths in vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
@@ -9,6 +10,7 @@ const host = process.env.TAURI_DEV_HOST;
 // https://vite.dev/config/
 export default defineConfig(async () => ({
 	plugins: [
+		tsconfigPaths(),
 		tanstackRouter({
 			target: "react",
 			autoCodeSplitting: true,
