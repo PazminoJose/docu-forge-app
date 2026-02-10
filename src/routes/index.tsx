563369a2ffc43cccc2c126368ca602a07c452df7
José Pazmiño <josepf.dev@gmail.com>
2026-02-10 17:40:20 -0500

    chore: remove unused beforeLoad function from file route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { invoke } from "@tauri-apps/api/core";
 import FileSelectionForm from "./@componentes/FileSelectionForm/FileSelectionForm";
 
 export const Route = createFileRoute("/")({
 	component: Index,
-	beforeLoad: async () => {
-		await invoke("clear_app_data");
-	},
 });
 
 function Index() {
