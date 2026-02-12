a1b012204ba6e274447aa7815f781849c462f601
José Pazmiño <josepf.dev@gmail.com>
2026-02-12 16:32:52 -0500

    feat: enhance DocxFields component with improved mapping functionality and UI updates

--- src/routes/create-document/@components/DocxFields.tsx
+++ src/routes/create-document/@components/DocxFields.tsx
@@ -1,15 +1,14 @@
-import { ActionIcon, TextInput, Tooltip } from "@mantine/core";
+import { ActionIcon, Button, TextInput, Tooltip } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { modals } from "@mantine/modals";
 import { useAppState } from "@stores/app.store";
-import { IconTablePlus, IconX } from "@tabler/icons-react";
+import { IconFileFilled, IconTablePlus, IconX } from "@tabler/icons-react";
 import { invoke } from "@tauri-apps/api/core";
 import { callPython } from "@utils/callPython";
-import { parseNumberToColumn } from "@utils/parseNumberToColumn";
 import { zod4Resolver } from "mantine-form-zod-resolver";
 import { useCallback, useEffect, useState } from "react";
-import { docxFieldsSchema, initialDocxFields } from "./docxFieldSchema";
-import SpreadsheetMapper from "./SpreadsheetMapper";
+import { DocxFieldsSchema, docxFieldsSchema, initialDocxFields } from "./docxFieldSchema";
+import SpreadsheetMapper, { type Mapping, type Range } from "./SpreadsheetMapper";
 
 export default function DocxFields() {
 	const [loading, setLoading] = useState(false);
@@ -27,21 +26,37 @@ export default function DocxFields() {
 			const endpoint = `show-data?path=${encodedPath}`;
 			const { data, range } = await callPython<{
 				data: string[][];
-				range: { from: number; to: number };
+				range: Range;
 			}>("GET", endpoint);
+			const initialMappedToColumn = form.getValues().fields[index].mappedToColumn;
+			const initialRange = form.getValues().fields[index].range;
+			let initialSelection: Mapping | null = null;
+			if (initialMappedToColumn && initialRange && initialRange.from && initialRange.to) {
+				initialSelection = {
+					column: initialMappedToColumn,
+					range: initialRange as Range,
+				};
+			}
 			modals.open({
 				size: "auto",
 				title: "Mapear a columna",
 				children: (
 					<SpreadsheetMapper
+						initialSelection={initialSelection}
 						data={data}
 						range={range}
-						onColumnSelect={(columnIndex) => {
-							if (columnIndex == null) {
+						onSave={(saved) => {
+							const { column, range } = saved;
+							if (column == null) {
 								form.setFieldValue(`fields.${index}.range`, undefined);
 								form.setFieldValue(`fields.${index}.mappedToColumn`, "");
 							} else {
-								form.setFieldValue(`fields.${index}.mappedToColumn`, parseNumberToColumn(columnIndex));
+								form.setFieldValue(`fields.${index}.mappedToColumn`, column);
+								form.setFieldValue(`fields.${index}.range`, range);
+								form.setFieldValue(
+									`fields.${index}.value`,
+									`${column}${range.from}:${column}${range.to}`,
+								);
 							}
 						}}
 					/>
@@ -55,6 +70,10 @@ export default function DocxFields() {
 	const handleRemoveMapping = (index: number) => {
 		form.setFieldValue(`fields.${index}.mappedToColumn`, "");
 		form.setFieldValue(`fields.${index}.range`, undefined);
+	};
+
+	const handleSubmit = async (values: DocxFieldsSchema) => {
+		console.log(values);
 	};
 
 	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
@@ -79,48 +98,61 @@ export default function DocxFields() {
 	}, [getVariables]);
 
 	return (
-		<form className="mt-2 flex flex-col gap-3">
-			{loading
-				? "Cargando campos..."
-				: form.getValues().fields.map((item, i) => {
-						const mappedColumn = form.getValues().fields[i].mappedToColumn;
-						const identifier = form.getValues().fields[i].identifier;
-						return (
-							<div key={item.identifier} className="flex flex-col gap-1">
-								<TextInput
-									disabled
-									label={`Campo ${identifier}`}
-									placeholder="Ingresa el valor o mapea a una columna"
-									{...form.getInputProps(`fields.${i}.value`)}
-								/>
-								<div className="flex gap-2">
-									<Tooltip
-										label={mappedColumn ? `Columna mapeada: ${mappedColumn}` : "Ninguna columna mapeada"}
-									>
-										<ActionIcon
-											onClick={() => handleMapToColumn(i)}
-											variant={mappedColumn ? "filled" : "outline"}
-											color="green"
-											aria-label="Settings"
+		<form
+			className="mt-2 flex h-full flex-col justify-between gap-3"
+			onSubmit={form.onSubmit(handleSubmit)}
+		>
+			<div>
+				{loading
+					? "Cargando campos..."
+					: form.getValues().fields.map((item, i) => {
+							const mappedColumn = form.getValues().fields[i].mappedToColumn;
+							const range = form.getValues().fields[i].range;
+							const identifier = form.getValues().fields[i].identifier;
+							return (
+								<div key={item.identifier} className="flex flex-col gap-1">
+									<TextInput
+										disabled
+										label={`Campo ${identifier}`}
+										placeholder="Ingresa el valor o mapea a una columna"
+										{...form.getInputProps(`fields.${i}.value`)}
+									/>
+									<div className="flex gap-2">
+										<Tooltip
+											label={
+												mappedColumn
+													? `Columna mapeada: ${mappedColumn}${range?.from}:${mappedColumn}${range?.to}`
+													: "Ninguna columna mapeada"
+											}
 										>
-											<IconTablePlus className="h-[70%] w-[70%]" stroke={1.5} />
-										</ActionIcon>
-									</Tooltip>
-									{mappedColumn && (
-										<Tooltip label="Quitar mapeo">
 											<ActionIcon
-												onClick={() => handleRemoveMapping(i)}
-												color="red"
+												onClick={() => handleMapToColumn(i)}
+												variant={mappedColumn ? "filled" : "outline"}
+												color="green"
 												aria-label="Settings"
 											>
-												<IconX className="h-[70%] w-[70%]" stroke={1.5} />
+												<IconTablePlus className="h-[70%] w-[70%]" stroke={1.5} />
 											</ActionIcon>
 										</Tooltip>
-									)}
+										{mappedColumn && (
+											<Tooltip label="Quitar mapeo">
+												<ActionIcon
+													onClick={() => handleRemoveMapping(i)}
+													color="red"
+													aria-label="Settings"
+												>
+													<IconX className="h-[70%] w-[70%]" stroke={1.5} />
+												</ActionIcon>
+											</Tooltip>
+										)}
+									</div>
 								</div>
-							</div>
-						);
-					})}
+							);
+						})}
+			</div>
+			<Button type="submit" leftSection={<IconFileFilled />}>
+				Generar
+			</Button>
 		</form>
 	);
 }
