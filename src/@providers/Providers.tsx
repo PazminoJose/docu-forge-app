0a8212bce2038984ef6884d283599b2f4957baf4
José Pazmiño <josepf.dev@gmail.com>
2026-02-24 12:31:12 -0500

    chore: restructure Providers component to include QueryClient and ReactQueryDevtools

--- src/@providers/Providers.tsx
+++ src/@providers/Providers.tsx
@@ -1,17 +1,32 @@
 import { MantineProvider } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ContextMenuProvider } from "mantine-contextmenu";
 import type { ReactNode } from "react";
 
 interface ProvidersProps {
 	children: ReactNode;
 }
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			staleTime: Infinity,
+		},
+	},
+});
+
 export default function Providers({ children }: ProvidersProps) {
 	return (
-		<MantineProvider>
-			<ContextMenuProvider>
-				<ModalsProvider>{children}</ModalsProvider>
-			</ContextMenuProvider>
-		</MantineProvider>
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={false} />
+			<MantineProvider>
+				<ContextMenuProvider>
+					<ModalsProvider>{children}</ModalsProvider>
+				</ContextMenuProvider>
+			</MantineProvider>
+		</QueryClientProvider>
 	);
 }
