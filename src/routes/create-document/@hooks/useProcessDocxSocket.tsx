73ef4a48d34cfc4d152172bdb0f58663392674c5
José Pazmiño <josepf.dev@gmail.com>
2026-02-24 12:31:57 -0500

    chore: add useProcessDocxSocket hook for WebSocket communication and document processing

---
+++ src/routes/create-document/@hooks/useProcessDocxSocket.tsx
@@ -0,0 +1,64 @@
+import { useAppState } from "@stores/app.store";
+import { useRef, useState } from "react";
+import { envs } from "src/@config/envs";
+import type { DocxFieldsSchema } from "../@components/DocxFields/docxFieldSchema";
+
+export default function useProcessDocxSocket() {
+	const templateFilePath = useAppState((state) => state.templateFilePath);
+	const dataFilePath = useAppState((state) => state.dataFilePath);
+	const outputFolderPath = useAppState((state) => state.outputFolderPath);
+	const [loading, setLoading] = useState(false);
+	const [progress, setProgress] = useState(0);
+	const socketRef = useRef<WebSocket | null>(null);
+
+	const processDocx = (values: DocxFieldsSchema) => {
+		socketRef.current = new WebSocket(`${envs.WEB_SOCKET_URL}/process-docx`);
+		if (socketRef.current != null) {
+			socketRef.current.onopen = () => {
+				setLoading(true);
+				socketRef.current?.send(
+					JSON.stringify({
+						action: "start",
+						docx_path: templateFilePath,
+						xlsx_path: dataFilePath,
+						output_folder: outputFolderPath,
+						skip_header: values.skipHeader,
+						range: values.range,
+						fields: values.fields,
+					}),
+				);
+			};
+
+			socketRef.current.onmessage = (event) => {
+				const data = JSON.parse(event.data);
+				if (data.status === "progress") {
+					setProgress(data.percent);
+				} else if (data.status === "completed") {
+					setLoading(false);
+					socketRef.current?.close();
+				} else if (data.status === "cancelled") {
+					console.warn("Proceso cancelado por el usuario");
+					setLoading(false);
+					socketRef.current?.close();
+					setProgress(0);
+				}
+			};
+		}
+	};
+
+	function cancelProcess() {
+		if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
+			setProgress(0);
+			setLoading(false);
+			socketRef.current.close();
+		}
+	}
+
+	return {
+		loading,
+		socketRef,
+		progress,
+		processDocx,
+		cancelProcess,
+	};
+}
