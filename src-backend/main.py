ee33283843dcd8f61adbb23a14d362fe37789258
José Pazmiño <josepf.dev@gmail.com>
2026-02-27 15:43:15 -0500

    feat: add support for unique combinations and multiple docx processing

--- src-backend/main.py
+++ src-backend/main.py
@@ -1,11 +1,14 @@
 from sanic import Sanic, Request, Websocket
-from services.service import get_mapper_data, get_mapper_range, get_docx_fields,get_docx_file
-from websockets_events.websocket import process_docx
-from config.cors import add_cors_headers
+from services.service import get_mapper_data, get_mapper_range, get_docx_fields,get_docx_file,get_unique_combinations
+from websockets_events.websocket import process_docx, process_multiple_docx
+from sanic_ext import Extend
 
 app = Sanic("PythonBackend")
 
-app.register_middleware(add_cors_headers, "response")
+app.config.CORS_ORIGINS = "*"
+app.config.CORS_METHODS = ["GET", "POST", "OPTIONS"]
+
+Extend(app)
 
 @app.get("/get-docx-fields")
 async def get_docx_fields_route(request: Request):
@@ -22,10 +25,18 @@ async def get_data(request: Request):
 @app.get("/get-range")
 async def get_range(request: Request):
     return await get_mapper_range(request)
+
+@app.post("/unique-combinations")
+async def unique_combinations_route(request: Request):
+    return await get_unique_combinations(request)
     
 @app.websocket("/process-docx")
 async def process_docx_route(_: Request, ws: Websocket):
     await process_docx(ws)
+
+@app.websocket("/process-multiple-docx")
+async def process_multiple_docx_route(_: Request, ws: Websocket):
+    await process_multiple_docx(ws)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8000, debug=False, fast=False, workers=1)
